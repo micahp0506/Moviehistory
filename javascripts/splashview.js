0a4a77ef6ac72c6c91e19fcd5a1d15eeb2db698a
mdhays <mdhays1995@gmail.com>
2015-11-19 15:56:35 -0600

    Initial commit.

---
+++ javascripts/splashview.js
@@ -0,0 +1,13 @@
+
+define(["jquery"], function($) {
+return {
+    splashViewDisplay: function(data) {
+      require(['hbs!../templates/splashview'], function (splashView){
+        //jquery to grab the dom element where we want to put our songs
+        //use songTemplate on data to generate html
+        //insert html into dom
+        $("#content").html(splashView(data));
+      });
+    }
+  };
+});
