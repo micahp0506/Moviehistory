4f1bdc8eba564c48be49d124a46a1c978bd3c0e3
Robert (Bobby) Ayers <rsheldon3ayers@gmail.com>
2015-11-19 14:03:06 -0600

    fixed template

---
+++ javascripts/config.js
@@ -0,0 +1,19 @@
+require.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'firebase': '../lib/bower_components/firebase/firebase',
+    'q': '../lib/bower_components/q/q',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'es6': '../lib/bower_components/requirejs-babel/es6',
+    'babel': '../lib/bower_components/requirejs-babel/babel-5.8.22.min'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
+  }
+});
---
+++ javascripts/dependencies.js
@@ -0,0 +1,7 @@
+define(function(require) {
+  var $ = require("jquery");
+  var b = require("bootstrap");
+  var h = require("hbs");
+  var fb = require("firebase");
+  var dom = require("dom");
+});
