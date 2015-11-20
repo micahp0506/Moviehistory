9a1291a6f831d4ef31f4eff429000afdc170af30
Robert (Bobby) Ayers <rsheldon3ayers@gmail.com>
2015-11-20 09:07:01 -0600

    made changes in findMovie.js

--- javascripts/findMovie.js
+++ javascripts/findMovie.js
@@ -8,26 +8,45 @@ define(function (require) {
 			
 			$("#find-movie").on("click", function() {
 
+			console.log("find-movie working");
 
-				console.log("find-movie working");
+				//need to get the value of move title
+				var title = $("#new-movie").val();
+				console.log("title", title);
+				//search the data base for the title
+				$.ajax({
+			      url: "http://img.omdbapi.com/?apikey=[yourkey]&"
+			    })
+			    .done(function(response) {
+			      // Resolve the promise
+			    })
+			    .fail(function(xhr, status, error) {
+			      // Reject the promise
+			      //end of ajax
+			      });
+
+			    
+			    //end of event handler
+			    
+			    });
 				
 
 
 
 				
-				//end of event handler
-			});
+			//end of FindMovie	
+			}
 
 
 
-       //end of FindMovie
-		}
+       //end of return
+		};
 
 
 
 
-		//end of return
-	};
+		
+	
 
 
 
