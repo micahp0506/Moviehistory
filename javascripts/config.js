4bc02b083b5750a31d30b8df489c0a637daa865f
mdhays <mdhays1995@gmail.com>
2015-11-22 16:55:02 -0600

    resolved merge conflicts with handlebarstw.

--- javascripts/config.js
+++ javascripts/config.js
@@ -19,16 +19,12 @@ require.config({
   }
 });
 
-<<<<<<< HEAD
+
 
 require(["dependencies", "splashview", "hbs!../templates/splashview", "findMovie", "authentication"], 
   function(_$_,  splashview, splashViewTemplate, findMovie, authentication) {
 
-=======
-require(
-  ["dependencies", "findMovie", "splashview", "hbs!../templates/splashview"], 
-  function(_$_, findMovie, splashview, splashViewTemplate) {
->>>>>>> handlebarstw
+
     
 
     splashview.splashViewDisplay();
